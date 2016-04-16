2d48456743081c8f6d060458393311495a1bff65
Shaan Sheikh <shaansweb@gmail.com>
2016-04-16 15:36:15 -0400

    indents

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ app.secret_key="A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    print json.loads(request.data)
-    fromid = json.loads(request.data)["messages"][0]["from"]
-    print fromid
+	print json.loads(request.data)
+	fromid = json.loads(request.data)["messages"][0]["from"]
+	print fromid
 
 	a = requests.post(
-	    'https://api.kik.com/v1/message',
-	    auth=('shaanbot', 'a024ef5e-627c-4166-9fb1-2093cba4f544'),
-	    headers={
-	        'Content-Type': 'application/json'
-	    },
-	    data=json.dumps({
-	        'messages': [
-	            {
-	                'body': 'I like ike', 
-	                'to': fromid, 
-	                'type': 'text'
-	            }
-	        ]
-	    })
+		'https://api.kik.com/v1/message',
+		auth=('shaanbot', 'a024ef5e-627c-4166-9fb1-2093cba4f544'),
+		headers={
+			'Content-Type': 'application/json'
+		},
+		data=json.dumps({
+			'messages': [
+				{
+					'body': 'I like ike', 
+					'to': fromid, 
+					'type': 'text'
+				}
+			]
+		})
 	)
 	return "hi"
 
